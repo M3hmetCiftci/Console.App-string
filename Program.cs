dc1218e4c9a0026bbc379a1868625c335911918a
M3hmetCiftci <Mehmet.cift27@gmail.com>
2023-10-28 20:35:32 +0300

    Console.App-string

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Sehir;
+            Sehir = "Istanbul";
+            Console.Write(Sehir);
+            Console.Read();
+
+
+
+            //string Sehir,Ilce;
+            //Sehir = "Istanbul";
+            //Ilce = "Merkez";
+            //Console.Write(Sehir+Ilce);
+            //Console.Read();
+
+
+            //string Sehir;
+            //string Ilce;
+            //Sehir = "Istanbul";
+            //Ilce = "Merkez";
+            //Console.Write(Sehir + Ilce);
+            //Console.Read();
+
+            //string Sehir;
+            //string Ilce;
+            //Sehir = "Istanbul";
+            //Ilce = "Merkez";
+            //Console.WriteLine(Sehir);
+            //Console.WriteLine(Ilce);
+            //Console.Read();
+
+            //string Sehir;
+            //string Ilce;
+            //Sehir = "Şehir: Istanbul";
+            //Ilce = "İlçe: Merkez";
+            //Console.WriteLine(Sehir);
+            //Console.WriteLine(Ilce);
+            //Console.Read();
+
+        }
+    }
+}
